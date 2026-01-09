openbmc/dbus-sensors

ExternalSensor: fix Units configuration
ExternalSensor configuration should use "DegreesC" for Units,
but setInitialProperties() requires full D-Bus
format (e.g., "xyz.openbmc_project.Sensor.Value.Unit.DegreesC").

Add convertToFullUnits() to convert short format to full D-Bus
format using static map lookup before setting properties.

Change-Id: I8716225ee9a991a221e1783bcca1b42425b6b2c2
Signed-off-by: Freddie Jheng <Freddie.Jheng@quantatw.com>

src/SensorPaths.cpp
@@ -12,41 +12,12 @@ namespace sensor_paths
 
 std::string getPathForUnits(const std::string& units)
 {
-    if (units == "DegreesC" || units == unitDegreesC)
+    for (const auto& unit : UNITS_MAP)
     {
-        return "temperature";
-    }
-    if (units == "RPMS" || units == unitRPMs)
-    {
-        return "fan_tach";
-    }
-    if (units == "Volts" || units == unitVolts)
-    {
-        return "voltage";
-    }
-    if (units == "Meters" || units == unitMeters)
-    {
-        return "altitude";
-    }
-    if (units == "Amperes" || units == unitAmperes)
-    {
-        return "current";
-    }
-    if (units == "Watts" || units == unitWatts)
-    {
-        return "power";
-    }
-    if (units == "Joules" || units == unitJoules)
-    {
-        return "energy";
-    }
-    if (units == "Percent" || units == unitPercent)
-    {
-        return "utilization";
-    }
-    if (units == "Pascals" || units == unitPascals)
-    {
-        return "pressure";
+        if (unit.dbusUnitName == units || unit.dbusPathName == units)
+        {
+            return std::string(unit.emUnitName);
+        }
     }
     return "";
 }
@@ -55,5 +26,18 @@ std::string escapePathForDbus(const std::string& name)
 {
     return std::regex_replace(name, std::regex("[^a-zA-Z0-9_/]+"), "_");
 }
+
+std::string convertToFullUnits(const std::string& units)
+{
+    for (const auto& unit : UNITS_MAP)
+    {
+        if (unit.dbusUnitName == units)
+        {
+            return std::string(unit.dbusPathName);
+        }
+    }
+    return "";
+}
+
 
 } // namespace sensor_paths

SivakamiVelusamy/CPP_Projects_Sysytem

src/SensorPaths.cpp
@@ -12,41 +12,12 @@ namespace sensor_paths
 
 std::string getPathForUnits(const std::string& units)
 {
-    if (units == "DegreesC" || units == unitDegreesC)
+    for (const auto& unit : unitsMap)
     {
-        return "temperature";
-    }
-    if (units == "RPMS" || units == unitRPMs)
-    {
-        return "fan_tach";
-    }
-    if (units == "Volts" || units == unitVolts)
-    {
-        return "voltage";
-    }
-    if (units == "Meters" || units == unitMeters)
-    {
-        return "altitude";
-    }
-    if (units == "Amperes" || units == unitAmperes)
-    {
-        return "current";
-    }
-    if (units == "Watts" || units == unitWatts)
-    {
-        return "power";
-    }
-    if (units == "Joules" || units == unitJoules)
-    {
-        return "energy";
-    }
-    if (units == "Percent" || units == unitPercent)
-    {
-        return "utilization";
-    }
-    if (units == "Pascals" || units == unitPascals)
-    {
-        return "pressure";
+        if (unit.dbusUnitName == units || unit.dbusPathName == units)
+        {
+            return std::string(unit.emUnitName);
+        }
     }
     return "";
 }
@@ -54,6 +25,18 @@ std::string getPathForUnits(const std::string& units)
 std::string escapePathForDbus(const std::string& name)
 {
     return std::regex_replace(name, std::regex("[^a-zA-Z0-9_/]+"), "_");
+}
+
+std::string convertToFullUnits(const std::string& units)
+{
+    for (const auto& unit : unitsMap)
+    {
+        if (unit.dbusUnitName == units)
+        {
+            return std::string(unit.dbusPathName);
+        }
+    }
+    return "";
 }
 
 } // namespace sensor_paths